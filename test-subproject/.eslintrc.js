20767f84b4415009d7e07884492ca7514145fe44
colin969 <colinb969@gmail.com>
2020-06-17 14:31:07 +0100

    test

--- test-subproject/.eslintrc.js
+++ test-subproject/.eslintrc.js
@@ -4,16 +4,23 @@ module.exports = {
         "es6": true
     },
     "extends": [
-        "standard"
+        "standard",
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended"
     ],
     "globals": {
         "Atomics": "readonly",
         "SharedArrayBuffer": "readonly"
     },
+    "root": true,
+    "parser": "@typescript-eslint/parser",
     "parserOptions": {
         "ecmaVersion": 2018,
         "sourceType": "module"
     },
     "rules": {
-    }
+    },
+    "plugins": [
+        "@typescript-eslint"
+    ],
 };
